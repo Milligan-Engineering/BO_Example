aa5d590f67422d2dff0ed83d3a623bb8d33fe72a
Jeffrey Giesey <jjgiesey@milligan.edu>
2019-02-06 09:02:48 -0500

    Added loop to read and store player names

--- BO_Example/BO_Example.cpp
+++ BO_Example/BO_Example.cpp
@@ -6,52 +6,50 @@
 //Last Changed: 02/04/2019
 
 #include <iostream>
+#include <string>
 using namespace std;
 //The Original Comment
 int main()
 {
 	int numberOfPlayers;
-	int turnTimeMinutes;
-	int turnTimeSeconds;
-	double turnTime;
 	int value;
+	int playerNumber;
 	const int MINPLAYERS = 2;
 	const int MAXPLAYERS = 6;
-	
+	string playerName[6];
+
 	cout << "Welcome to the B&O Banker Assistant \n";
 
-	// Retrieve and validate number of players using do while
-	do
-	{
-		cout << "How many players will be playing? Must be between "<< MINPLAYERS << " and " << MAXPLAYERS << " players." ;
-		cin >> numberOfPlayers;
-	} 
-	while ((numberOfPlayers < MINPLAYERS) || (numberOfPlayers > MAXPLAYERS));
-	cout << "There will be " << numberOfPlayers << " players.\n"; // Echo number of players
-	
-	/* Validating the number of players using a while satement requires more
-	lines but it could be clearer to the user of the program. */
+	// Retrieve and validate number of players using while
+
 	cout << "How many players will be playing?";
-	cin >> numberOfPlayers;	
-	while ((numberOfPlayers < MINPLAYERS) || (numberOfPlayers > MAXPLAYERS)) 
+	cin >> numberOfPlayers;
+	while ((numberOfPlayers < MINPLAYERS) || (numberOfPlayers > MAXPLAYERS))
 	{
 		cout << "Number of players must be between " << MINPLAYERS << " and " << MAXPLAYERS << ". Please enter number again.";
 		cin >> numberOfPlayers;
 	}
 	cout << "There will be " << numberOfPlayers << " players.\n";// Echo number of players
-		
-	cout << "Enter the desired minutes of turn time: ";
-	cin >> turnTimeMinutes;
 
-	cout << "Enter the desired seconds of turn time: ";
-	cin >> turnTimeSeconds;
+	playerNumber = 1;
+	while (playerNumber <= numberOfPlayers)
+	{
+		cout << "Enter name of Player " << playerNumber << ": ";
+		cin >> playerName[playerNumber];
+		playerNumber++;
+	}
 
-	turnTime = turnTimeMinutes + turnTimeSeconds/60.;
+	cout << "There will be " << numberOfPlayers << " playing. \n";
+	playerNumber = 1;
+	while (playerNumber <= numberOfPlayers)
+	{
+		cout <<  playerNumber << ": " << playerName[playerNumber] << endl;
+		playerNumber++;
+	}
 
-	cout << "There will be " << numberOfPlayers << " playing with a turn time of " 
-		<< turnTimeMinutes << " minutes and " << turnTimeSeconds << " seconds. \n";
 	cout << "Enter character to continue \n";
 	cin >> value;
-	// This is a different comment
+
+	// Additional comment
 	return(0);
 }
