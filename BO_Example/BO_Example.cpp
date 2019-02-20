0dbe1cdf1b1152207f04316ae4fd46a007bb1430
Jeffrey Giesey <jjgiesey@milligan.edu>
2019-02-20 08:24:01 -0500

    More changes

--- BO_Example/BO_Example.cpp
+++ BO_Example/BO_Example.cpp
@@ -4,7 +4,7 @@
 //Term Project
 //Description: This program handles banking duties in the B&O Board Game
 // Modified to contain multiple loop problems
-//Last Changed: 02/12/2019
+//Last Changed: 02/18/2019
 
 #include <iostream>
 #include <string>
@@ -61,11 +61,62 @@ int main()
 		cout << i << ": " << playerName[i] << endl;
 
 	}
-
-
 	
 	cout << "Enter character to continue \n";
 	cin >> value;
-	// A Better Comment
+
 	return(0);
+}
+
+int setupPlayers(string playerName[], int playerCash[], int playerTurnorder[])
+{
+	// Set up players names
+	int numberPlayers = 0;
+
+	while ((numberPlayers < 3) || (numberPlayers > 6)) // Check for valid input. If not - keep asking
+	{
+		cout << "Enter number of players between 3 and 6. \n";
+		cin >> numberPlayers;
+	}
+	cout << "There will be " << numberPlayers << " players. \n";
+
+
+	for (int i = 0; i < numberPlayers; i++) // Get name for each player
+	{
+		cout << "What is the name of the player " << i << "? \n";
+		cin >> playerName[i];
+		cout << "Player " << i + 1 << " is " << playerName[i] << endl << endl; // echoing input for validation
+	}
+
+
+	// Set random initial turn order
+	long int currentTime = static_cast<long int>(time(0)); //Generate random seed
+	srand(currentTime);
+
+	for (int i = 0; i < numberPlayers; i++)
+	{
+		playerTurnorder[i] = i;	// assign initial turn order
+		playerCash[i] = rand() % 10000; // assign randome number
+	}
+
+
+	// list players in turnorder
+	cout << "Name \t Cash\t Order\t \n";
+	PrintInformation(playerName, playerCash, playerTurnorder, numberPlayers);
+
+	//Sort players by cash
+	int test = setPlayerTurnorder(playerCash, playerTurnorder, numberPlayers);
+
+	// Set up initial player cash and net worth
+	for (int i = 0; i < numberPlayers; i++)
+	{
+		playerCash[i] = totalInitialCash / numberPlayers;
+	}
+
+	// list players in turnorder
+	cout << "Name \t Cash\t Order\t \n";
+	PrintInformation(playerName, playerCash, playerTurnorder, numberPlayers);
+
+	return(numberPlayers);
+
 }
